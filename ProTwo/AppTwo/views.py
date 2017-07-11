d7a7f46f8430e5adc634a991e109559ee5dc22ea
sujitsarkar0991 <sujitsarkar91@gmail.com>
2017-07-11 11:48:22 +0530

    commiting the first assignment

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import User
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("AppTwo")
+    return render(request, "AppTwo/index.html")
+
+def user(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict ={'users':user_list}
+    return render(request, "AppTwo/users.html", context = user_dict)
 
 def help(request):
     dict = {"insert_me":"Help page"}
